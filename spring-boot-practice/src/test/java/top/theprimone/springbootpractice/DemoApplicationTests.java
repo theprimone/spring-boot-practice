b04c7d7e1e0703be18cf19a8627b152bce5e3731
Yuns Xie <yuns.xie@qq.com>
2020-02-11 12:13:36 +0800

    feat: test of bean.Admin

--- spring-boot-practice/src/test/java/top/theprimone/springbootpractice/DemoApplicationTests.java
+++ spring-boot-practice/src/test/java/top/theprimone/springbootpractice/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package top.theprimone.springbootpractice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import top.theprimone.springbootpractice.bean.Admin;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	Admin admin;
+
 	@Test
 	void contextLoads() {
+		System.out.println(admin);
 	}
 
 }
