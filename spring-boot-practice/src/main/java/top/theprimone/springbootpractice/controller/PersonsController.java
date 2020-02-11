8ea143bdc649277ebbb4503118f6275929f3197e
Yuns Xie <yuns.xie@qq.com>
2020-02-11 23:18:46 +0800

    feat: init PersonsController

---
+++ spring-boot-practice/src/main/java/top/theprimone/springbootpractice/controller/PersonsController.java
@@ -0,0 +1,52 @@
+package top.theprimone.springbootpractice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import top.theprimone.springbootpractice.bean.Message;
+import top.theprimone.springbootpractice.bean.Person;
+
+@RestController
+public class PersonsController {
+
+  private static List<Person> persons = new ArrayList<>();
+  
+  static {
+    Person aa = new Person();
+    aa.setName("AA");
+    aa.setUsername("AA");
+    persons.add(aa);
+
+    Person bb = new Person();
+    bb.setName("BB");
+    bb.setUsername("BB");
+    persons.add(bb);
+  }
+
+  @RequestMapping(value = "/persons", method = RequestMethod.GET)
+  public ResponseEntity<Object> getPersons() {
+    return new ResponseEntity<>(persons, HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/persons", method = RequestMethod.POST)
+  public ResponseEntity<Object> createPerson(@RequestBody Person person) {
+    persons.add(person);
+    return new ResponseEntity<>("person is created successfully", HttpStatus.CREATED);
+  }
+
+  @RequestMapping(value = "/persons/{name}", method = RequestMethod.GET)
+  public ResponseEntity<Object> getPersonByName(@PathVariable("name") String name) {
+    List<Person> result = persons.stream().filter((Person person) -> person.getName().equals(name)).collect(Collectors.toList());
+    return new ResponseEntity<>(!result.isEmpty() ? result : new Message("Not found", "No reference"), HttpStatus.OK);
+  }
+
+}
